feabe7c44b1cfaa9cd6bfca0cb87dcdd358770a5
Joakimhp <52169074+Joakimhp@users.noreply.github.com>
2023-06-05 11:39:40 +0200

    Init setup

---
+++ API/Controllers/RecipesController.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class RecipesController : BaseApiController
+{
+    private readonly CookbookContext _context;
+
+    public RecipesController(CookbookContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<List<Recipe>>> GetRecipes()
+    {
+        var recipes = await _context.Recipes.ToListAsync();
+        return Ok(recipes);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Recipe>> GetRecipe(int id)
+    {
+        var recipe = await _context.Recipes.FindAsync(id);
+        return Ok(recipe);
+    }
+}
---
+++ Infrastructure/Data/CookbookContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class CookbookContext : DbContext
+{
+    public CookbookContext(DbContextOptions<CookbookContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Recipe> Recipes { get; set; } = null!;
+}
