dd9d640e15062e5345c0d91f20312cb45a0488a2
Joakimhp <52169074+Joakimhp@users.noreply.github.com>
2023-06-05 11:46:03 +0200

    Added preparation and cooking time to Recipe.cs

--- Core/Entities/Recipe.cs
+++ Core/Entities/Recipe.cs
@@ -5,5 +5,6 @@ public class Recipe
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    
+    public int preparationMinutes { get; set; }
+    public int cookingMinutes { get; set; }
 }
